63a703f40522b8e28460e6b79b7f39b91ea1f71a
Erofeev Kirill <kirieshka2350@gmail.com>
2021-10-15 13:26:56 +0700

    initial, do a work

---
+++ Arch/MBC.h
@@ -0,0 +1,13 @@
+#ifndef MyBigChars
+#define MyBigChars
+#include "MT.h"
+
+int ba_printA(char *str);
+int bc_box(int x1, int y1, int x2, int y2);
+int bc_printbigchar(int *big, int x, int y, enum colors, enum colors);
+int bc_setbigcharpos(int *big, int x, int y, int value);
+int bc_getbigcharpos(int *big, int x, int y, int *value);
+int bc_bigcharwrite(int fd, int *big, int count);
+int bc_bigcharread(int fd, int *big, int need_cout, int *count);
+
+#endif // !MyBigChars
